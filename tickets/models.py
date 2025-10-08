2450303d0d39c3fe83c784414106cc54dd761ca7
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2025-10-08 12:19:46 +0300

    create tickets models and serializers

---
+++ tickets/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+# Guest Movie Reservation
+
+
+class Movie(models.Model):
+    hall = models.CharField(max_length=10)
+    movie = models.CharField(max_length=20)
+    date = models.DateField()
+
+
+class Guest(models.Model):
+    name = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10)
+
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, related_name='reservation', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='reservation', on_delete=models.CASCADE)
+    
+
+
