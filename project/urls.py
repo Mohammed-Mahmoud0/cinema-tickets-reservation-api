9338cd2bcc259ed8790d715c7f72ce62b11e5676
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-10-14 21:44:19 +0300

    create class based view

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path("django/jsonresponsenomodel/", views.no_rest_no_model),
     # 2
     path("django/jsonresponsefrommodel/", views.no_rest_from_model),
-    # 3 GET POST from rest framework function based view
+    # 3.1 GET POST from rest framework function based view
     path("rest/fbv/", views.FBV_List),
+    # 3.2 GET PUT DELETE from rest framework function based view
+    path("rest/fbv/<int:pk>", views.FBV_PK),
+    
+    # 4.1 GET POST from rest framework class based view
+    path("rest/cbv/", views.CBV_List.as_view()),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from .serializers import GuestSerializer, MovieSerializer, ReservationSerializer
 from rest_framework import status, filters
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -24,29 +25,69 @@ def no_rest_from_model(request):
     response = {"guests": list(data.values("name", "mobile"))}
     return JsonResponse(response)
 
+
 # List == GET
 # Create == POST
 # PK query == GET
 # Update == PUT
 # Delete == DELETE
 
+
 # 3 function based views
 # 3.1 GET POST
 @api_view(["GET", "POST"])
 def FBV_List(request):
-  # GET
-  if request.method == "GET":
-    guests = Guest.objects.all()
-    serializer = GuestSerializer(guests, many=True)
-    return Response(serializer.data)
-  # POST
-  elif request.method == "POST":
-    serializer = GuestSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    # GET
+    if request.method == "GET":
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+    # POST
+    elif request.method == "POST":
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
 
 # 3.1 GET UPDATE DELETE
-# @api_view()
-# def
+@api_view(["GET", "PUT", "DELETE"])
+def FBV_PK(request, pk):
+    try:
+        guest = Guest.objects.get(pk=pk)
+    except Guest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    # GET
+    if request.method == "GET":
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data)
+    # Put
+    elif request.method == "PUT":
+        serializer = GuestSerializer(guest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # DELETE
+    if request.method == "DELETE":
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# CBV class based views
+# 4.1 list and create == GET and POST
+class CBV_List(APIView):
+    def get(self, request):
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
